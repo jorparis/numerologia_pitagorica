1f128c4e1100a8a2e36f93d75558c4485048836d
jorparis <pajjorge@gmail.com>
2025-12-14 12:38:31 -0500

    Update mappings.rb

--- lib/numerologia_pitagorica/mappings.rb
+++ lib/numerologia_pitagorica/mappings.rb
@@ -1,7 +1,9 @@
+# frozen_string_literal: true
+
 module NumerologiaPitagorica
   module Mappings
-    # Mapeo Pitagórico estándar (1-9)
-    LETTER_VALUES = {
+    # Tabla de conversión letras a números (Pitagórica)
+    LETRA_A_NUMERO = {
       'A' => 1, 'J' => 1, 'S' => 1,
       'B' => 2, 'K' => 2, 'T' => 2,
       'C' => 3, 'L' => 3, 'U' => 3,
@@ -10,11 +12,37 @@ module NumerologiaPitagorica
       'F' => 6, 'O' => 6, 'X' => 6,
       'G' => 7, 'P' => 7, 'Y' => 7,
       'H' => 8, 'Q' => 8, 'Z' => 8,
-      'I' => 9, 'R' => 9
+      'I' => 9, 'R' => 9,
+      # Letras con acentos
+      'Á' => 1, 'É' => 5, 'Í' => 9, 'Ó' => 6, 'Ú' => 3,
+      'Ñ' => 5
     }.freeze
 
-    # Definiciones para cálculos avanzados
-    VOWELS = %w[A E I O U].freeze
-    CONSONANTS = %w[B C D F G H J K L M N P Q R S T V W X Y Z].freeze
+    # Vocales (para número del alma)
+    VOCALES = %w[A E I O U Á É Í Ó Ú].freeze
+
+    # Consonantes (para número de personalidad)
+    CONSONANTES = %w[
+      B C D F G H J K L M N Ñ P Q R S T V W X Y Z
+    ].freeze
+
+    # Números maestros (no se reducen)
+    NUMEROS_MAESTROS = [11, 22, 33].freeze
+
+    def self.es_vocal?(letra)
+      VOCALES.include?(letra.upcase)
+    end
+
+    def self.es_consonante?(letra)
+      CONSONANTES.include?(letra.upcase)
+    end
+
+    def self.letra_a_numero(letra)
+      LETRA_A_NUMERO[letra.upcase] || 0
+    end
+
+    def self.es_numero_maestro?(numero)
+      NUMEROS_MAESTROS.include?(numero)
+    end
   end
 end
